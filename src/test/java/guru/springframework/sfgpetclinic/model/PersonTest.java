a33e53c2d40d7ef9a4471c15f02850ec268c0c9a
Caroline Nunes <carolinenunes@Carolines-MacBook-Pro.local>
2020-05-30 17:15:42 +1000

    Grouped assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,32 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void groupedAssertions(){
+        //given
+        Person person = new Person(1l, "Joe", "Buck");
+
+        //then
+        assertAll("Group Assertion",
+        ()-> assertEquals("Joe",person.getFirstName() ),
+        ()->assertEquals("Buck",person.getLastName()));
+    }
+
+    @Test
+    void groupedAssertionsMessages(){
+        //given
+        Person person = new Person(1l, "Joe", "Buck");
+
+        //then
+        assertAll("Group Assertion",
+                ()-> assertEquals("Joe",person.getFirstName(), "First Name Failed"),
+                ()->assertEquals("Buck",person.getLastName(),  "Last name Failed"));
+    }
+
+}
