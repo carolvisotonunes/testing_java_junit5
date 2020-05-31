377ae7d908438dcf4d563d90def96e7c1d39c1c3
Caroline Nunes <carolinenunes@Carolines-MacBook-Pro.local>
2020-05-31 17:03:23 +1000

    dependentAssertions- Ownser pojo

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class OwnerTest {
+    @Test
+    void dependentAssertions() {
+        Owner owner = new Owner(1l, "Joe", "Buck");
+        owner.setCity("City");
+        owner.setTelephone("123123123");
+        assertAll("Properties Test",
+                () -> assertAll("Person Properties",
+                        () -> assertEquals("Joe", owner.getFirstName(), "First Name did not match"),
+                        () -> assertEquals("Buck", owner.getLastName())),
+                () -> assertAll("Owner Properties",
+                        () -> assertEquals("City", owner.getCity(),"City name did not match"),
+                        () -> assertEquals("123123123", owner.getTelephone()))
+        );
+    }
+
+}
