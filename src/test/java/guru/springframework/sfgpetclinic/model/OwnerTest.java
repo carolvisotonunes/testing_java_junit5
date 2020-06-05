24672b61a597824d552ab680c1a7edfc66df4609
Caroline Nunes <carolinenunes@Carolines-MacBook-Pro.local>
2020-06-05 16:33:49 +1000

    parameterized method provider not working

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -6,10 +6,10 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -63,4 +63,18 @@ class OwnerTest implements ModelTest {
     void csvFileTest(String stateName, int val1, int val2) {
         System.out.println(stateName +" = "+ val1 +" : "+ val2);
     }
+
+
+    @DisplayName("Method Provider Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {argumentsWithNames}")
+    @MethodSource("getArgs")
+    void fromMethodTest(String stateName, int val1, int val2) {
+        System.out.println(stateName +" = "+ val1 +" : "+ val2);
+    }
+
+//    static Stream<Arguments> getArgs(){
+//        return Stream.of(Arguments.of("FL", 1,1),
+//                Arguments.of("OH", 2,2), Arguments.of("MI", 1,1));
+//
+//    }
 }
